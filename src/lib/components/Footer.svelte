559b99947fe59d85eeff7dc1e395f16e18b32c17
5204402 <f.leenders@uu.nl>
2026-05-10 16:25:37 +0200

    feat: WIP footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,19 +1,22 @@
-<footer class="bg-red-1 flex flex-wrap">
-  <div class="flex flex-wrap">
-    <div class="flex flex-column">
+<footer class="text-text-light bg-red-1 flex flex-wrap">
+  <div class="flex flex-wrap gap-12">
+    <div class="flex flex-col">
       <h2>Contact</h2>
     </div>
-    <div class="flex flex-column">
+    <div class="flex flex-col">
       <h2>Standpunten</h2>
+      <a href="/verkiezingsprogramma#"></a>
+      <a href="/verkiezingsprogramma#"></a>
+      <a href="/verkiezingsprogramma#"></a>
     </div>
-    <div class="flex flex-column">
+    <div class="flex flex-col">
       <h2>Onze mensen</h2>
-      <a href="onze-mensen/#universiteitsraad">Universiteitsraad</a>
-      <a href="onze-mensen/#faculteit-BETA">Faculteit BETA</a>
-      <a href="onze-mensen/#faculteit-GW">Faculteit GW</a>
-      <a href="onze-mensen/#faculteit-REBO">Faculteit REBO</a>
-      <a href="onze-mensen/#faculteit-FSW">Faculteit FSW</a>
-      <a href="onze-mensen/#bestuur">Bestuur</a>
+      <a href="/onze-mensen#universiteitsraad">Universiteitsraad</a>
+      <a href="/onze-mensen#faculteit-BETA">Faculteit BETA</a>
+      <a href="/onze-mensen#faculteit-GW">Faculteit GW</a>
+      <a href="/onze-mensen#faculteit-REBO">Faculteit REBO</a>
+      <a href="/onze-mensen#faculteit-FSW">Faculteit FSW</a>
+      <a href="/onze-mensen#bestuur">Bestuur</a>
     </div>
   </div>
 </footer>
