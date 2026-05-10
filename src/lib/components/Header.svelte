3e57edb588147fa5d63a1eb8496c109ad1eabbae
5204402 <f.leenders@uu.nl>
2026-05-10 17:05:47 +0200

    feat: WIP continue header footer

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
-
+    import logo from "$lib/assets/logo/logo_color_dark.svg";
 
 </script>
 
 
-<header>
+<header class="w-full h-17.5 md:h-20 xl:h-25 px-6 md:px-16 xl:px-24 py-2 xl:px-24 xl:py-16 bg-bg-light text-text-dark flex flex-row justify-between">
+<img src={logo} class="h-full"/>
+<div class="flex flex-row gap-12">
+    <a>Home</a>
+    <a>Election programme</a>
+    <a>Our people</a>
+</div>
 
     
 </header>
