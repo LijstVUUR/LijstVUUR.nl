c9ce6f920431e490299403b241a37b72fe4da985
5204402 <f.leenders@uu.nl>
2026-05-10 19:17:54 +0200

    feat: WIP mobile header. Styling could be better

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,18 +1,42 @@
 <script lang="ts">
     import logo from "$lib/assets/logo/logo_color_dark.svg";
-    import hamburger from "$lib/assets/icons/hamburgerMenu.svg";
+  import Hamburger from "$lib/assets/icons/Hamburger.svelte";
+  import MenuClose from "$lib/assets/icons/MenuClose.svelte";
 
 </script>
 
 
-<header class="w-full h-17.5 md:h-20 xl:h-25 px-6 md:px-16 xl:px-24 py-2 xl:px-24 bg-bg-light text-text-dark flex flex-row justify-between items-center">
+<header class="  box-border w-full h-16 md:h-20 lg:h-24 px-6 md:px-16 lg:px-24 py-2 bg-bg-light text-text-dark flex flex-row justify-between items-center">
     <img src={logo} alt="VUUR logo full color" class="h-full w-auto"/>
-    <div class="flex flex-row gap-12">
-        <ul class="font-extrabold uppercase flex flex-row gap-12 items-center">
-            <li><a>Home</a></li>
-            <li><a>Election programme</a></li>
-            <li><a>Our people</a></li>
+    <nav class="flex flex-row gap-12">
+        <input id="menu-active" type="checkbox" class="peer hidden">
+        <label for="menu-active" class="h-11 aspect-square lg:hidden block peer-checked:hidden">
+            <Hamburger />
+        </label>
+        <label for="menu-active" class="h-11 aspect-square lg:hidden hidden peer-checked:block">
+            <MenuClose />
+        </label>
+        <div class="hidden 
+        peer-checked:block
+        peer-checked:box-border
+        peer-checked:w-full
+        peer-checked:absolute
+        peer-checked:inset-x-0
+        peer-checked:inset-y-16
+        peer-checked:md:inset-y-20
+        peer-checked:py-16
+        peer-checked:bg-red-1
+        peer-checked:text-text-light
+        peer-checked:z-10
+        lg:block ">
+        <ul class="font-extrabold uppercase flex 
+        flex-col
+        lg:flex-row 
+        gap-12 items-center">
+            <li><a href="/">Home</a></li>
+            <li><a href="/election-programme">Election programme</a></li>
+            <li><a href="/our-people">Our people</a></li>
         </ul>
-        <img src={hamburger} alt="hamburger menu icon" class="h-11 aspect-square xl:hidden"/>
-    </div>
+        </div>
+    </nav>
 </header>
