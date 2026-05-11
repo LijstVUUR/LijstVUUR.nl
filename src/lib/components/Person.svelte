33643cba5ca8c7b77f59c005084713cfdb7de384
Maurits Wilke <mauritswilke@gmail.com>
2026-05-12 00:03:09 +0200

    feat: add people

---
+++ src/lib/components/Person.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+  import Instagram from "$lib/assets/icons/social/Instagram.svelte";
+  import LinkedIn from "$lib/assets/icons/social/LinkedIn.svelte";
+  import UU from "$lib/assets/icons/social/UU.svelte";
+  import { languageTag } from "$lib/paraglide/runtime";
+
+  type Person = {
+    name: string;
+    degree: {
+      nl: string;
+      en: string;
+    };
+    img_src: string;
+    linkedin: string;
+    instagram: string;
+    utrecht: string;
+  };
+  let { variant = "border", person }: { variant?: "border" | "borderless"; person: Person } = $props();
+
+  let degree_localised = $derived(person.degree[languageTag()]);
+</script>
+
+<div id="wrapper" class="w-full rounded overflow-hidden h-[500px] flex flex-col bg-bg-beige">
+  <img src={person.img_src} alt={`Picture of ${person.name}`} class="h-full object-cover object-top" />
+
+  <div class="flex flex-col gap-2 flex-1 p-4">
+    <div class="flex flex-col gap-1">
+      <h3>{person.name}</h3>
+      <p>{degree_localised}</p>
+    </div>
+    <div class="flex gap-2">
+      {#if person.linkedin}
+        <a href={person.linkedin}>
+          <div class="w-6 h-6">
+            <LinkedIn />
+          </div>
+        </a>
+      {/if}
+
+      {#if person.instagram}
+        <a href={person.instagram}>
+          <div class="w-6 h-6">
+            <Instagram />
+          </div>
+        </a>
+      {/if}
+
+      {#if person.utrecht}
+        <a href={person.utrecht}>
+          <div class="w-6 h-6">
+            <UU />
+          </div>
+        </a>
+      {/if}
+    </div>
+  </div>
+</div>
--- src/routes/our-people/+page.svelte
+++ src/routes/our-people/+page.svelte
@@ -1,1 +1,58 @@
-our people yay
+<script lang="ts">
+  import Person from "$lib/components/Person.svelte";
+  import * as People from "$lib/assets/OurPeople.json";
+  import Block from "$lib/components/Block.svelte";
+  import ButtonLink from "$lib/components/ButtonLink.svelte";
+
+  type Person = {
+    name: string;
+    degree: { nl: string; en: string };
+    img_src: string;
+    linkedin: string;
+    instagram: string;
+    utrecht: string;
+  };
+
+  const u_council = People.u_council as any as Person[];
+  const beta = People.beta_faculty as any as Person[];
+  const gw = People.gw_faculty as any as Person[];
+  const rebo = People.rebo_faculty as any as Person[];
+  const fsw = People.fsw_faculty as any as Person[];
+
+  const full_list = [
+    { title: "Universiteitsraad", description: "Description yet to come", people: u_council, scroll: "u_council" },
+    { title: "Faculteit BETA", description: "Description yet to come", people: beta, scroll: "beta" },
+    { title: "Faculteit GW", description: "Description yet to come", people: gw, scroll: "gw" },
+    { title: "Faculteit REBO", description: "Description yet to come", people: rebo, scroll: "rebo" },
+    { title: "Faculteit FSW", description: "Description yet to come", people: fsw, scroll: "fsw" },
+  ];
+
+  function scrollTo(id: string) {
+    document.getElementById(id)?.scrollIntoView({ behavior: "smooth" });
+  }
+</script>
+
+<Block>
+  <div class="flex flex-wrap gap-2 pt-16">
+    <ButtonLink onclick={() => scrollTo("u_council")} colour="secondary"><p>Universiteitsraad</p></ButtonLink>
+    <ButtonLink onclick={() => scrollTo("beta")} colour="secondary"><p>Faculteit BETA</p></ButtonLink>
+    <ButtonLink onclick={() => scrollTo("gw")} colour="secondary"><p>Faculteit GW</p></ButtonLink>
+    <ButtonLink onclick={() => scrollTo("rebo")} colour="secondary"><p>Faculteit REBO</p></ButtonLink>
+    <ButtonLink onclick={() => scrollTo("fsw")} colour="secondary"><p>Faculteit FSW</p></ButtonLink>
+  </div>
+
+  {#each full_list as list}
+    <div class="flex flex-col gap-8 pt-8 pb-16">
+      <div class="flex flex-col gap-1">
+        <h1 id={list.scroll}>{list.title}</h1>
+        <p>{list.description}</p>
+      </div>
+
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {#each list.people as member}
+          <Person person={member} />
+        {/each}
+      </div>
+    </div>
+  {/each}
+</Block>
