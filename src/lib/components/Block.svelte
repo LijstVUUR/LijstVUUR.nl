8c1f6030bc52054d02adbd4811a8d5e9e25b3db3
fleeeur <fleur.leenders01@gmail.com>
2026-05-11 22:08:11 +0200

    fix: max width on content

--- src/lib/components/Block.svelte
+++ src/lib/components/Block.svelte
@@ -4,6 +4,8 @@
   let { children }: { children: Snippet } = $props();
 </script>
 
-<div class="px-6 md:px-16 xl:px-24">
-  {@render children?.()}
+<div class="px-6 md:px-16 xl:px-24 flex flex-col items-center">
+  <div class="w-full max-w-[1400px]">
+    {@render children?.()}
+  </div>
 </div>
