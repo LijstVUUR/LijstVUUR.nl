ab3b7cf09a421663692f2f7921be9e40aaf05c8d
Maurits Wilke <mauritswilke@gmail.com>
2026-05-11 21:45:46 +0200

    feat: add kernwaarden ft. translations

---
+++ src/lib/components/Accordion.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+  import DropdownChip from "$lib/assets/icons/DropdownChip.svelte";
+  import { slide } from "svelte/transition";
+
+  type Elem = { title: string; content: string; img_src: string; img_alt: string };
+  let { elements = [] }: { elements: Elem[] } = $props();
+
+  // svelte-ignore state_referenced_locally
+  // reasoning: the elements wont ever change
+  let open = $state<boolean[]>(elements.map(() => false));
+  open[0] = true;
+  let current = $derived(elements[0]);
+
+  function toggle(i: number) {
+    if (open[i]) {
+      open[i] = false;
+      return;
+    }
+    elements.map((_, j) => {
+      open[j] = j == i;
+      if (j == i) current = elements[j];
+    });
+  }
+</script>
+
+<div class="flex gap-12">
+  <div class="divide-y divide-[#CCCCCC] flex-1">
+    {#each elements as elem, i}
+      <div>
+        <button
+          onclick={() => toggle(i)}
+          class="flex w-full justify-between px-4 py-4 rounded-t items-center transition-colors
+               {open[i] ? 'bg-primary' : 'bg-transparent'}"
+          aria-expanded={open[i]}
+          aria-controls="accordion-body-{i}"
+        >
+          <h3 class={open[i] ? "text-text-light" : "text-text-dark"}>{elem.title}</h3>
+          <span class="m-2 {open[i] ? 'text-white' : 'text-text-dark'} transition-transform duration-200 {open[i] ? 'rotate-180' : 'rotate-0'}">
+            <DropdownChip />
+          </span>
+        </button>
+
+        {#if open[i]}
+          <div transition:slide={{ duration: 250 }} id="accordion-body-{i}" class="flex flex-col px-4 py-4 gap-6">
+            <p class="max-w-[75ch] leading-7">{@html elem.content}</p>
+            <img
+              src={elem.img_src}
+              alt={elem.img_alt}
+              class="w-full h-[350px] object-cover object-center
+         md:w-auto md:h-auto md:max-h-[475px] md:object-contain
+         lg:hidden
+         rounded"
+            />
+          </div>
+        {/if}
+      </div>
+    {/each}
+  </div>
+
+  <div class="hidden lg:block flex-1 w-full max-w-[375px]">
+    <img src={current.img_src} alt={current.img_alt} class="w-full h-full object-cover object-center rounded" />
+  </div>
+</div>
---
+++ src/routes/election-programme/+page.svelte
@@ -0,0 +1,63 @@
+<script>
+  import Accordion from "$lib/components/Accordion.svelte";
+  import Block from "$lib/components/Block.svelte";
+  import Button from "$lib/components/Button.svelte";
+
+  import * as m from "$lib/paraglide/messages.js";
+
+  const elements = [
+    {
+      title: m.footer_4_rule(),
+      content: m.kernwaarden_4_rule_content(),
+      img_src: "/images_unopt/4_rule.png",
+      img_alt: "Person writing with a pen on paper",
+    },
+    {
+      title: m.footer_inclusivity(),
+      content: m.kernwaarden_inclusive_policy_content(),
+      img_src: "/images_unopt/inclusive_policy.png",
+      img_alt: "Person writing with a pen on paper",
+    },
+    {
+      title: m.footer_digital_autonomy(),
+      content: m.kernwaarden_digital_autonomy_content(),
+      img_src: "/images_unopt/digital_autonomy.png",
+      img_alt: "Person writing with a pen on paper",
+    },
+    {
+      title: m.footer_end_bsa(),
+      content: m.kernwaarden_end_bsa_content(),
+      img_src: "/images_unopt/end_bsa.png",
+      img_alt: "Person writing with a pen on paper",
+    },
+    {
+      title: m.footer_protect_protesting(),
+      content: m.kernwaarden_protect_protesting_content(),
+      img_src: "/images_unopt/protect_protesting.png",
+      img_alt: "Person writing with a pen on paper",
+    },
+  ];
+</script>
+
+<Block>
+  <div class="pt-16 pb-8">
+    <h1>{m.kernwaarden_header()}</h1>
+
+    <div class="flex flex-col gap-16">
+      <div>
+        <Accordion {elements}></Accordion>
+      </div>
+
+      <div class="flex flex-col gap-10 justify-center align-middle">
+        <h2 class="text-center leading-9">{m.kernwaarden_read_program()}</h2>
+
+        <!-- BEETJE KUT MAAR LATER PAS FIXEN  -->
+        <embed src="/Election Programme 2026-2027.pdf" type="application/pdf" width="100%" height="600px" />
+
+        <Button>
+          <p>Download het programma</p>
+        </Button>
+      </div>
+    </div>
+  </div>
+</Block>
