d506ba8c9728bc8385f6b5cc872db377ac159c9c
Maurits Wilke <mauritswilke@gmail.com>
2026-05-20 13:00:23 +0200

    feat: add verenigingvuur to the footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -7,28 +7,16 @@
   import * as m from "$lib/paraglide/messages.js";
 </script>
 
-<footer
-  class="flex-0 text-text-light bg-red-1 flex flex-col xl:flex-row flex-wrap items-start justify-between gap-4 w-full py-12 px-12 md:px-20 xl:px-24 xl:py-16"
->
+<footer class="flex-0 text-text-light bg-red-1 flex flex-col xl:flex-row flex-wrap items-start justify-between gap-4 w-full py-12 px-12 md:px-20 xl:px-24 xl:py-16">
   <img src={logo} alt="VUUR logo white with colored flame" class="h-full" />
   <div class="flex flex-wrap gap-x-12 gap-y-8">
     <div class="flex flex-col gap-2">
       <h2>{m.footer_contact()}</h2>
-      <a
-        href={localizeHref("mailto:bestuur@verenigingvuur.nl")}
-        class="underline">bestuur@verenigingvuur.nl</a
-      >
+      <a href={localizeHref("mailto:bestuur@verenigingvuur.nl")} class="underline">bestuur@verenigingvuur.nl</a>
+      <a target="_blank" href="https://verenigingvuur.nl" class="underline">VerenigingVUUR.nl</a>
       <div class="flex gap-4">
-        <a
-          class="w-11 h-11"
-          target="_blank"
-          href="https://www.linkedin.com/company/vuur"><LinkedIn /></a
-        >
-        <a
-          class="w-11 h-11"
-          target="_blank"
-          href="https://www.instagram.com/vuurlijst"><Instagram /></a
-        >
+        <a class="w-11 h-11" target="_blank" href="https://www.linkedin.com/company/vuur"><LinkedIn /></a>
+        <a class="w-11 h-11" target="_blank" href="https://www.instagram.com/vuurlijst"><Instagram /></a>
       </div>
     </div>
     <div class="flex flex-col">
@@ -45,9 +33,7 @@
       <h2>{m.footer_people()}</h2>
       <ul class="flex flex-col gap-2">
         <li>
-          <a href={localizeHref("/our-people#u_council")}
-            >{m.footer_ucouncil()}</a
-          >
+          <a href={localizeHref("/our-people#u_council")}>{m.footer_ucouncil()}</a>
         </li>
         <li>
           <a href={localizeHref("/our-people#beta")}>{m.footer_beta()}</a>
